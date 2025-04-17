d5215f5be9f9df6083644d755aa36d9f22dc2306
fkyyy <fikri107aja@gmail.com>
2025-04-17 13:37:47 +0700

    validap

--- Application/Common/Dtos/Attendances/AbsenDto.cs
+++ Application/Common/Dtos/Attendances/AbsenDto.cs
@@ -6,24 +6,24 @@ namespace Application.Common.Dtos.Attendances;
 
 public class AbsenDto
 {
-    
+    [Required]
     public Guid IdUser { get; set; }
 
-    
+    [Required]
     public DateTime Date { get; set; }
 
-    
+    [Required]
     public DateTimeOffset CheckIn { get; set; }
 
-    
+    [Required]
     public string Status { get; set; } = string.Empty;
 
     public string Notes { get; set; } = string.Empty;
 
-    
+    [Required]
     public string IsApproved { get; set; } = string.Empty;
 
-    
+    [Required]
     public EnumType AttendanceType { get; set; } = EnumType.Sakit; 
 
     public Guid? AttachmentId { get; set; }
--- Application/Common/Dtos/Divisions/CreateDivisionDto.cs
+++ Application/Common/Dtos/Divisions/CreateDivisionDto.cs
@@ -4,6 +4,6 @@ namespace Application.Common.Dtos.Divisions;
 
 public class CreateDivisionDto
 {
-    
+    [Required]
     public string DivisionName { get; set; } = string.Empty;
 }
